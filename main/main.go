2f235baf58acb8c064139d40316e06841d035c47
birowo <1340zxcvbnm@gmail.com>
2025-09-09 11:31:26 +0700

    commit

--- main/main.go
+++ main/main.go
@@ -7,21 +7,28 @@ import (
 )
 
 func main() {
-	x := []byte("QWERTYUIOPASDFGHJKLZXCVBNMqwertyui")
+	x := []byte("QWERTYUIOPASDFGHJKLZXCVBNMqwerty")
 	xLen := len(x)
 	y := make([]byte, b64.StdEncoding.EncodedLen(xLen))
-	base64.Encode(y, x)
 	z := make([]byte, xLen)
-	n, _ := base64.Decode(z, y)
-	println("x:", string(x), "y:", string(y), "z:", string(z[:n]))
-	if string(x) != string(z[:n]) {
-		println("FAIL")
+
+	for i := 24; i < 33; i++ {
+		j := b64.StdEncoding.EncodedLen(i)
+		base64.Encode(y[:j], x[:i])
+		n, _ := b64.StdEncoding.Decode(z[:i], y[:j])
+		println("x:", string(x[:i]), "y:", string(y[:j]), "z:", string(z[:n]))
+		if string(x[:i]) != string(z[:n]) {
+			println("FAIL")
+		}
 	}
-	b64.StdEncoding.Encode(y, x)
-	n, _ = b64.StdEncoding.Decode(z, y)
-	println("x:", string(x), "y:", string(y), "z:", string(z[:n]))
-	if string(x) != string(z[:n]) {
-		println("FAIL")
+	println()
+	for i := 24; i < 33; i++ {
+		j := b64.StdEncoding.EncodedLen(i)
+		b64.StdEncoding.Encode(y[:j], x[:i])
+		n, _ := base64.Decode(z[:i], y[:j])
+		println("x:", string(x[:i]), "y:", string(y[:j]), "z:", string(z[:n]))
+		if string(x[:i]) != string(z[:n]) {
+			println("FAIL")
+		}
 	}
-	println("yLen:", len(y))
 }
